5e74c912d2bc119dbaa50dc17e28d601f6ed3e39
Nilay <pandaynilay@gmail.com>
2024-02-26 14:30:59 +0530

    Complete color changer project

---
+++ color_changer-project/index.js
@@ -0,0 +1,31 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "pink") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "violet") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "orange") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "black") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
